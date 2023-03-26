b7334535cf22050800244a30669a198afe987f47
nawfel <sghaier.nawfel@hotmail.com>
2023-03-26 23:19:12 +0200

    add client blazor app

---
+++ GestionConge.UI/Services/LeaveRequestService.cs
@@ -0,0 +1,13 @@
+﻿using GestionConge.UI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace GestionConge.UI.Services
+{
+    public class LeaveRequestService : BaseHttpService, ILeaveRequestService
+    {
+        public LeaveRequestService(IClient client) : base(client)
+        {
+        }
+    }
+    
+}
---
+++ GestionConge.UI/Services/LeaveTypeService.cs
@@ -0,0 +1,14 @@
+﻿using Blazored.LocalStorage;
+using GestionConge.UI.Contracts;
+using HR.LeaveManagement.BlazorUI.Services.Base;
+
+namespace GestionConge.UI.Services
+{
+    public class LeaveTypeService : BaseHttpService, ILeaveTypeService
+    {
+        public LeaveTypeService(IClient client) : base(client)
+        {
+        }
+    }
+    
+}
